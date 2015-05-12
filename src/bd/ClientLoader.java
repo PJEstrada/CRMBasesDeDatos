612896ec61143ae4431a60a458221e984c4b4aed
ernestoivan22 <ernestoivan22@hotmail.com>
2015-05-12 11:42:32 -0600

    cosos de neto

---
+++ src/bd/ClientLoader.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+/**
+ *
+ * @author Al
+ */
+public class ClientLoader {
+    
+    
+    //Metodo que se encarga de realizar la defernsiva de un campo determinado
+    //Recibe un identificador y un texto ingresado
+    //Los identificadores seran:
+    /*
+        1 = String
+        2 = Integer
+        3 = Float
+        4 = Date
+    */
+    //public boolean     
+}
